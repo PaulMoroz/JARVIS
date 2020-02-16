e68db5024a120814202167239158b60c3aec5934
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-02-16 13:49:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import os
 import sys
 import webbrowser
 from espeak import espeak
+
+
+
+
 #================
 #Say something
 #================
 def talk(words):
 	espeak.synth(words)
+
+
 
 #================
 #recognizing program
@@ -22,16 +28,54 @@ def command():
 		audio = r.listen(sourse)
 
 	try:
-		text  = r.recognize_google(audio).lower()
+		task  = r.recognize_google(audio).lower()
 		print("You said: " + text)
 	except:
 		print("You said something misunderstanding!")
 		command = command()
-	return text
+	if "jarvis" in task:
+		return task
 
 
-def make_something(task):
+
+
+#=========================
+#Router tasks
+#=========================
+def router(task):
 	if task == 'exit':
+		talk("Ok, no problem")
 		sys.exit()
+	elif task == "turn off pc":
+		talk("goodbye")
+		os.system("poweroff")
+	elif 'open' in task:
+		open(task)
+	elif 'close' in task:
+		os.system("killall " + task[task.find('close')+len(5):])
+	else:
+		talk("That function is developing")
 
-command()
+
+
+#==============================
+#Open something
+#==============================
+def open(task):
+	talk("opening")
+	if 'app' in task:
+		keyword = 'app'
+		if 'sublime' in task:
+			t = os.system("subl")
+		elif "browser" in task:
+			t = os.system("firefox")
+		elif "player" in task:
+			t = os.system("rhythmbox")
+		else:
+			t = os.system(task[task.find(keyword)+len(keyword)+1:])	
+	elif 'folder' in task:
+		keyword = 'folder'
+		t = os.system("nautilus | " + task[task.find(keyword)+len(keyword)+1:])
+	else:
+		webbrowser.open_new_tab('http://google.com/?q='+task[task.find(keyword)+len(keyword)+1:])
+	talk("Opened")
